76a1ba0a86c11590512b003f744c0bc3d0273eca
Tong Lei <39419642+luyaz999@users.noreply.github.com>
2021-09-06 11:53:20 +0100

    SCWR_test
    Reference:
    Lei, T., & Sun, D. W. (2020). A novel NIR spectral calibration method: sparse coefficients wavelength selection and regression (SCWR). Analytica chimica acta, 1110, 169-180.

---
+++ GetParameters.m
@@ -0,0 +1,11 @@
+function [Hpar] = GetParameters()
+%GETPARAMEERS 此处显示有关此函数的摘要
+%   此处显示详细说明
+Hpar.Iter = 80; %
+Hpar.ro = 1.1; % 
+Hpar.mu = 0.0002; %
+Hpar.lm = 0.01;
+Hpar.lm2 = 0.03;
+Hpar.Ncomp = 11;
+end
+
